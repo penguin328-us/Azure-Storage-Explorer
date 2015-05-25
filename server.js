4df8a60c3a6f53b9317c51ca4623d2acd9330e38
penguin328 <zliyong@gmail.com>
2015-05-25 20:56:44 +0000

    redirect http to https

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-
+var http = require("http");
 var fs = require("fs");
 
 var table = require('./table.js');
 var blob = require('./blob.js');
+var httpsPort = 443;
 var port = process.env.PORT || 80;
 var ip = process.env.IP || "0.0.0.0";
 
@@ -10,38 +11,47 @@ var express = require('express');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var app  = express();
+
+var httpsOptions = null;
 var server;
 
 if(fs.existsSync('https.pfx')){
-    port = process.env.PORT || 443;
-    var https = require("https");
-    var option = {
+    httpsOptions = {
         pfx: fs.readFileSync('https.pfx')
     }
-    
-    server = https.createServer(option,app);
 }
 else if(fs.existsSync('https-key.pem') && fs.existsSync('https-cert.pem')){
-     port = process.env.PORT || 443;
-    var https = require("https");
-    var option = {
+    httpsOptions = {
         key: fs.readFileSync('https-key.pem'),
         cert: fs.readFileSync('https-cert.pem')
     }
-    
-    server = https.createServer(option,app);
 }
-else{
-    var http = require('http');
-    server = http.createServer(app);
-}
-
- 
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 app.use(cookieParser());
 table(app);
 blob(app);
+
+if(httpsOptions){
+    var https = require("https");
+    
+    var httpsServer = https.https.createServer(httpsOptions,app);
+    httpsServer.listen(httpsPort, ip, function(){
+        var addr = server.address();
+        console.log("Https server listening at", addr.address + ":" + addr.port);
+    });
+    
+    server = http.createServer(function(req,res){
+        // redirect to https
+        res.writeHead(301,{
+            Location: 'https://' + req.headers.host  + req.url
+        });
+        res.end();
+    });
+}
+else{
+    server = http.createServer(app);
+}
 
 server.listen(port, ip , function () {
     var addr = server.address();
