49879c6485b9f80643280d4e37d6c1e33665b2bc
penguin328 <zliyong@gmail.com>
2015-05-25 01:25:24 +0000

    add ssl support

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
-var http = require('http');
+
+var fs = require("fs");
+
 var table = require('./table.js');
 var blob = require('./blob.js');
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 80;
 var ip = process.env.IP || "0.0.0.0";
 
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var path = require('path');
 var app  = express();
-var server = http.createServer(app);
+var server;
+
+if(fs.existsSync('https.pfx')){
+    port = process.env.PORT || 443;
+    var https = require("https");
+    var option = {
+        pfx: fs.readFileSync('https.pfx')
+    }
+    
+    server = https.createServer(option,app);
+}
+else if(fs.existsSync('https-key.pem') && fs.existsSync('https-cert.pem')){
+     port = process.env.PORT || 443;
+    var https = require("https");
+    var option = {
+        key: fs.readFileSync('https-key.pem'),
+        cert: fs.readFileSync('https-cert.pem')
+    }
+    
+    server = https.createServer(option,app);
+}
+else{
+    var http = require('http');
+    server = http.createServer(app);
+}
+
+ 
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 app.use(cookieParser());
